051e1f8373b3547d9d4266ecccba42e8403c2a49
miiqbal <arizkyiqbal@gmail.com>
2022-04-24 07:40:19 +0700

    Add Navigation Bar and Fonts Setting

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFF67952);
+const Color bgColor = Color(0xFF07070A);
+const Color defaultCard = Color(0xFF262837);
+
+const double defaultPadding = 16.0;
+const double defaultBorderRadius = 12.0;
